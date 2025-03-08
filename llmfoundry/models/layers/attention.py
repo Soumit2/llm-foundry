c4ac51f848cf77945bf4bf622940dc526fc598c6
Soumit0702 <147476913+Soumit0702@users.noreply.github.com>
2025-03-08 21:25:48 +0530

    Update attention.py

--- llmfoundry/models/layers/attention.py
+++ llmfoundry/models/layers/attention.py
@@ -21,6 +21,13 @@ from llmfoundry.layers_registry import (
 from llmfoundry.models.layers.layer_builders import build_fc, build_norm
 from llmfoundry.models.utils.config_defaults import fc_type_defaults
 
+layer = build_fc(
+    name='BandLinear',
+    in_features=128,
+    out_features=64,
+    fc_kwargs={'bandwidth': 2, 'bias': True}
+)
+
 __all__ = [
     'scaled_multihead_dot_product_attention',
     'flash_attn_fn',
