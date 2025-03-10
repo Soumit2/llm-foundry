20bbefdf18a3b849fe0bcd008749c9d2c2efaf52
Soumit0702 <147476913+Soumit0702@users.noreply.github.com>
2025-03-10 09:20:43 +0530

    Update fc.py

--- llmfoundry/models/layers/fc.py
+++ llmfoundry/models/layers/fc.py
@@ -1,6 +1,29 @@
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
+
+# Copyright 2022 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
+
+# from torch import nn
+
+# from llmfoundry.layers_registry import fcs
+
+# fcs.register('torch', func=nn.Linear)
+
+# try:
+#     import transformer_engine.pytorch as te
+#     fcs.register('te', func=te.Linear)
+# except:
+#     pass
+
+# import torch
+# from torch import nn
+# from llmfoundry.layers_registry import fcs
+
+# Copyright 2022 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
+
 import torch
 from torch import nn
 from llmfoundry.layers_registry import fcs
@@ -8,17 +31,17 @@ from llmfoundry.layers_registry import fcs
 class BandMatrix(nn.Module):
     """A custom Band Matrix layer that retains only a banded structure."""
 
-    def __init__(self, in_features: int, out_features: int, bandwidth: int):
+    def __init__(self, in_features: int, out_features: int, **kwargs):
         """
         Args:
             in_features (int): Number of input features.
             out_features (int): Number of output features.
-            bandwidth (int): Maximum number of diagonals to retain.
+            kwargs (dict): Additional keyword arguments (e.g., bandwidth).
         """
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.bandwidth = bandwidth
+        self.bandwidth = kwargs.get("bandwidth", min(in_features, out_features) // 2)  # Default to half the size
 
         # Learnable weight matrix
         self.weight = nn.Parameter(torch.randn(out_features, in_features))
@@ -44,11 +67,5 @@ class BandMatrix(nn.Module):
         """
         return torch.matmul(x, (self.weight * self.band_mask).T)
 
-# Register the BandMatrix layer in fcs
-fcs.register(
-    "BandMatrix",
-    BandMatrix,
-    in_features=int,
-    out_features=int,
-    bandwidth=int,
-)
+# Register BandMatrix layer in fcs
+fcs.register('band_matrix', func=BandMatrix)
