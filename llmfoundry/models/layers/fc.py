c03154aedc60f6acb9b1a2717b56942b560692ae
Soumit0702 <147476913+Soumit0702@users.noreply.github.com>
2025-03-18 18:53:46 +0530

    Update fc.py

--- llmfoundry/models/layers/fc.py
+++ llmfoundry/models/layers/fc.py
@@ -5,17 +5,17 @@
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
-from torch import nn
+# from torch import nn
 
-from llmfoundry.layers_registry import fcs
+# from llmfoundry.layers_registry import fcs
 
-fcs.register('torch', func=nn.Linear)
+# fcs.register('torch', func=nn.Linear)
 
-try:
-    import transformer_engine.pytorch as te
-    fcs.register('te', func=te.Linear)
-except:
-    pass
+# try:
+#     import transformer_engine.pytorch as te
+#     fcs.register('te', func=te.Linear)
+# except:
+#     pass
 
 # import torch
 # from torch import nn
