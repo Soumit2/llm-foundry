061c052d4065a607b02cc5c9f83cc5c7b2bad390
Soumit0702 <147476913+Soumit0702@users.noreply.github.com>
2025-03-23 13:21:23 +0530

    Update fc.py

--- llmfoundry/models/layers/fc.py
+++ llmfoundry/models/layers/fc.py
@@ -1,6 +1,22 @@
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
+from torch import nn
+
+from llmfoundry.layers_registry import fcs
+
+fcs.register('torch', func=nn.Linear)
+
+try:
+    import transformer_engine.pytorch as te
+    fcs.register('te', func=te.Linear)
+except:
+    pass
+
+
+# Copyright 2022 MosaicML LLM Foundry authors
+# SPDX-License-Identifier: Apache-2.0
+
 
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
@@ -24,12 +40,12 @@
 # Copyright 2022 MosaicML LLM Foundry authors
 # SPDX-License-Identifier: Apache-2.0
 
-import torch
-import torch.nn as nn
-from llmfoundry.layers_registry import fcs
+# import torch
+# import torch.nn as nn
+# from llmfoundry.layers_registry import fcs
 
-from llmfoundry.custom_layers import BandMatrix
-# from llmfoundry.custom_layers_toeplitz import ToeplitzMatrix
+# from llmfoundry.custom_layers import BandMatrix
+# # from llmfoundry.custom_layers_toeplitz import ToeplitzMatrix
 
-fcs.register('BandMatrix', func=BandMatrix)
-# fcs.register('toeplitz_matrix', func = ToeplitzMatrix)
+# fcs.register('BandMatrix', func=BandMatrix)
+# # fcs.register('toeplitz_matrix', func = ToeplitzMatrix)
